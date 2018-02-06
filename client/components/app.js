a856bd03d4ba09e2c2ca6e21d85adf1f0f9dd88b
Jon Preece <jonpreece@hotmail.co.uk>
2018-02-06 19:07:27 +0000

    Switched to glamorous

--- client/components/app.js
+++ client/components/app.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import classnames from 'classnames'
-import styles from './app.css'
+import Header from '../styles/h1'
 
-const classNames = classnames(
-  'mb-5',
-  styles.header
-)
 const App = props => {
   return (
     <div className='container'>
-      <h1 className={classNames}>
+      <Header>
         { props.prop }
-      </h1>
+      </Header>
       <h1>
         { props.prop }
       </h1>
---
+++ client/styles/h1.js
@@ -0,0 +1,7 @@
+import glamorous from 'glamorous'
+
+export default glamorous.h1({
+  fontSize: '2.5rem',
+  marginTop: 10,
+  color: '#CC3A4B'
+})
