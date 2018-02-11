6a4a0d56f5298543be1f1dd45433a0cadc07785d
Jon Preece <jonpreece@hotmail.co.uk>
2018-02-11 16:48:06 +0000

    Add jest for unit testing

---
+++ client/Components/Checklists/index.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from 'enzyme'
+import Checklists from './index'
+
+const renderComponent = () => {
+  return render(
+    <Checklists />
+  )
+}
+
+test('check that title is set', () => {
+  const component = renderComponent()
+
+  const header = component.find('h1').first().text()
+  expect(header).toBe('Checklists')
+})
