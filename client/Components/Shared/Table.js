8d125d7a89847fdc2667b64ba90196fc33f96277
Jon Preece <jonpreece@hotmail.co.uk>
2018-02-27 21:07:43 +0000

    Wired up table on shortlist page

--- client/Components/Shared/Table.js
+++ client/Components/Shared/Table.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
 
-import Table, { TableBody, TableCell, TableFooter, TablePagination, TableRow } from 'material-ui/Table'
+import Table, { TableHead, TableBody, TableCell, TableFooter, TablePagination, TableRow } from 'material-ui/Table'
 import Paper from 'material-ui/Paper'
 
 import TablePaginationActions from 'Shared/TablePaginationActions'
@@ -17,41 +17,26 @@ const styles = theme => ({
   },
   tableWrapper: {
     overflowX: 'auto'
+  },
+  hover: {
+    cursor: 'pointer'
   }
 })
 
-let counter = 0
-function createData(name, calories, fat) {
-  counter += 1
-  return { id: counter, name, calories, fat }
-}
-
-class CustomPaginationActionsTable extends React.Component {
+class MaterialTable extends React.Component {
   constructor(props, context) {
     super(props, context)
 
     this.state = {
-      data: [
-        createData('Cupcake', 305, 3.7),
-        createData('Donut', 452, 25.0),
-        createData('Eclair', 262, 16.0),
-        createData('Frozen yoghurt', 159, 6.0),
-        createData('Gingerbread', 356, 16.0),
-        createData('Honeycomb', 408, 3.2),
-        createData('Ice cream sandwich', 237, 9.0),
-        createData('Jelly Bean', 375, 0.0),
-        createData('KitKat', 518, 26.0),
-        createData('Lollipop', 392, 0.2),
-        createData('Marshmallow', 318, 0),
-        createData('Nougat', 360, 19.0),
-        createData('Oreo', 437, 18.0)
-      ].sort((a, b) => (a.calories < b.calories ? -1 : 1)),
+      data: props.data,
+      columns: props.columns,
       page: 0,
       rowsPerPage: 5
     }
 
     this.handleChangePage = this.handleChangePage.bind(this)
     this.handleChangeRowsPerPage = this.handleChangeRowsPerPage.bind(this)
+    this.handleRowClick = this.handleRowClick.bind(this)
   }
 
   handleChangePage(event, page) {
@@ -62,22 +47,38 @@ class CustomPaginationActionsTable extends React.Component {
     this.setState({ rowsPerPage: event.target.value })
   }
 
+  handleRowClick(event, selectedData) {
+    if (this.props.rowClicked) {
+      this.props.rowClicked(selectedData)
+    }
+  }
+
   render() {
     const { classes } = this.props
-    const { data, rowsPerPage, page } = this.state
+    const { data, columns, rowsPerPage, page } = this.state
     const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage)
 
     return (
       <Paper className={classes.root}>
         <div className={classes.tableWrapper}>
           <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                {columns.map((column, index) => {
+                  return <TableCell key={index}>{column}</TableCell>
+                })}
+              </TableRow>
+            </TableHead>
             <TableBody>
               {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => {
                 return (
-                  <TableRow key={n.id}>
-                    <TableCell>{n.name}</TableCell>
-                    <TableCell numeric>{n.calories}</TableCell>
-                    <TableCell numeric>{n.fat}</TableCell>
+                  <TableRow key={n.id} hover className={classes.hover} onClick={event => this.handleRowClick(event, n)}>
+                    {Object.keys(n).map((key, index) => {
+                      if (key === 'id') {
+                        return null
+                      }
+                      return <TableCell key={index}>{n[key]}</TableCell>
+                    })}
                   </TableRow>
                 )
               })}
@@ -107,8 +108,11 @@ class CustomPaginationActionsTable extends React.Component {
   }
 }
 
-CustomPaginationActionsTable.propTypes = {
-  classes: PropTypes.object.isRequired
+MaterialTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+  data: PropTypes.array,
+  columns: PropTypes.array,
+  rowClicked: PropTypes.func
 }
 
-export default withStyles(styles)(CustomPaginationActionsTable)
+export default withStyles(styles)(MaterialTable)
--- client/Components/Shortlist/index.js
+++ client/Components/Shortlist/index.js
@@ -1,9 +1,19 @@
 import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
 
 import Heading from 'Shared/Heading'
 import Table from 'Shared/Table'
 
-const Shortlist = () => {
+const columns = ['Reference', 'Address', 'Potential ROI']
+const data = [{ id: 1, reference: '3 bedroom house', address: '1 New Property Lane', potentialROI: '9.5%' }]
+
+const rowClicked = (selectedData, history) => {
+  if (selectedData) {
+    history.push(`/shell/shortlist/analyse/${selectedData.id}`)
+  }
+}
+
+const Shortlist = ({ history }) => {
   return (
     <Fragment>
       <Heading text="Shortlist" variant="heading-1" />
@@ -11,11 +21,13 @@ const Shortlist = () => {
         Easily track properties you are considering purchasing and see the potential ROI of each
       </Heading>
       <Heading variant="subheading">Click on the property reference for more information.</Heading>
-      <Table />
+      <Table data={data} columns={columns} rowClicked={selectedData => rowClicked(selectedData, history)} />
     </Fragment>
   )
 }
 
-Shortlist.propTypes = {}
+Shortlist.propTypes = {
+  history: PropTypes.object.isRequired
+}
 
 export default Shortlist
